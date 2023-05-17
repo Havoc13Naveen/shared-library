0222a83f5b80c0e3e5c76add112b21112d57e8bb
Naveen kumar S <108734259+Havoc13Naveen@users.noreply.github.com>
2023-05-17 10:49:58 +0530

    Update find.groovy

--- vars/find.groovy
+++ vars/find.groovy
@@ -1,9 +1,8 @@
 def call(name){
   sh '''
-  find /home/ubuntu/ -type d -mmin -20 -iname "${name}" >> log_file
-  cd /home/ubuntu/workspace/test-library/
-  cat log_file
+  find /home/ubuntu/ -type d -mmin -20  >> /home/ubuntu/log_file
   cd /home/ubuntu/
+  cat log_file
   cat script.sh
   '''
 }
