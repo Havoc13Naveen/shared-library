2ae61cbc3eca9831ac392029831eae7026d2b41a
Naveen kumar S <108734259+Havoc13Naveen@users.noreply.github.com>
2023-05-17 10:55:42 +0530

    Update path.groovy

--- vars/path.groovy
+++ vars/path.groovy
@@ -1,7 +1,7 @@
 def call(){
   sh '''
-  cd /home/ubuntu
-  top | head -20 | tail -1 >> top_log
+  cd /home/ubuntu/
+   ps -ef | head -20 | tail -1 >> top_log
   cat top_log
   rm -rf top_log
   '''
